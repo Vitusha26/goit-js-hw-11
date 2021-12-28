0188083406604d6aec9f23290d94005f392d76ba
Mukha Viktor <conor005777@gmail.com>
2021-12-28 23:14:13 +0200

    Dz

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
 import './sass/main.scss';
+import axios from 'axios';
+import pictureTpl from './partials/picture.hbs';
+import Notiflix from 'notiflix';
+let page = 1;
+let searchQuery = '';
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+searchForm.addEventListener('submit', onSubmit);
+// const input = document.getElementsByTagName('input').searchQuery;
+
+function onSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  searchQuery = form.elements.searchQuery.value;
+  gallery.innerHTML = '';
+  getPictures(searchQuery, 1);
+
+  function twoFunc () {
+      document.getElementById('load-butt').style.display="block";
+  }
+twoFunc()
+  
+}
+
+async function getPictures(searchQuery, page) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=24985308-45fb8285c0d4accdcde29835e&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`,
+    );
+    if (response?.data?.hits?.length) {
+      renderCards(response.data.hits);
+    } else {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    }
+    searchForm.reset();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function renderCards(pictures) {
+  pictures.forEach(pic => {
+    const markup = pictureTpl(pic);
+    gallery.innerHTML += markup;
+  });
+}
+
+loadMoreBtn.addEventListener('click', loadMore);
+
+function loadMore() {
+  page++;
+  gallery.innerHTML = '';
+  getPictures(searchQuery, page);
+}
